55bfe4acd28ef7d15a71e4bb2dbf0d33f870846f
Enrico Hartung <ehartung@users.noreply.github.com>
2017-06-22 08:51:05 +0200

    Activate connection TTL for Zuul and set default to 10 minutes (#1)

--- src/main/java/org/zalando/seaproxy/Application.java
+++ src/main/java/org/zalando/seaproxy/Application.java
@@ -2,7 +2,7 @@ package org.zalando.seaproxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
